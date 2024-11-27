6fa680e9d4578949b74b9ae2997b86d7e3c631d5
Эллина <Arlekino@mail.ru>
2024-11-27 21:10:47 +0300

    создание класса Restaurant и его методов

--- food-delivery.py
+++ food-delivery.py
@@ -17,3 +17,29 @@ class Menu:
     def print_menu(self,dish):
         print(dish)
         print(f"Состав блюда: {", ".join(self.ingredients)}\n")
+
+class Restaurant:
+    def __init__(self,title,menu):
+        self.title = title
+        self.menu = menu
+        self.pay_order = "Ожидание оплаты заказа"
+        self.status_order = "успешно принят"
+
+    def payment_order(self,client):
+        print(f"Статус оплаты заказа, клиента {client.name}:\n")
+        time.sleep(3)
+        print(f"{self.pay_order}")
+        self.pay_order = "Заказ оплачен"
+        time.sleep(3)
+        print(f"{self.pay_order}")
+        self.pay_order = "Оплата получена"
+        time.sleep(3)
+        print(f"{self.pay_order}")
+    
+    def accept_order(self,client):
+        print(f"Статус заказа клиента {client.name}:\n")
+        time.sleep(3)
+        print(f"Клиент {client.name}, ваш заказ {self.status_order}")
+        self.status_order = "успешно оформлен"
+        time.sleep(3)
+        print(f"Клиент {client.name}, ваш заказ {self.status_order}")
