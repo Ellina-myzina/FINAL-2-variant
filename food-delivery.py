bbe478e0bfd8aa5f4c4a8087601531c623a2a4bc
Эллина <Arlekino@mail.ru>
2024-11-27 21:09:00 +0300

    создание класса Menu и его методов

---
+++ food-delivery.py
@@ -0,0 +1,19 @@
+import time
+
+class Menu:
+    def __init__(self,category,name,price,weight,ingredients):
+        self.category = category
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.ingredients = ingredients
+    
+    def __str__(self):
+        return f"{self.category}: {self.name}({self.weight} г) - {self.price} руб."
+
+    def add_menu(self,menu,dish):
+        menu.append(dish)
+
+    def print_menu(self,dish):
+        print(dish)
+        print(f"Состав блюда: {", ".join(self.ingredients)}\n")
